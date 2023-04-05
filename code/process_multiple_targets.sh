bbf5d78d2eda93d4bf14db04693db9b43f933a5a
davidko89 <davidchanyoungko@gmail.com>
2023-04-05 09:08:25 +0900

    codes_updated(20230405)

--- code/process_multiple_targets.sh
+++ code/process_multiple_targets.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+# !/bin/sh
 
 target_column=$1 # label sev_cars sev_ados young_cars old_cars
 
 for task_num in 1 2 3
 do
-    python get_dataset.py $target_column $task_num 811
-    bash process_target_model.sh $target_column $task_num 811 5
+    python get_dataset.py $target_column sev_ados $task_num 811
+    bash process_target_model.sh $target_column sev_ados $task_num 811 10
 done
--- code/train_model.py
+++ code/train_model.py
@@ -26,15 +26,15 @@ target_fold_num = int(sys.argv[4]) # Under fold_num
 
 
 # 경로 및 Argument 설정
-ROOT_PATH    = Path('data/asd_jointattention')
+ROOT_PATH    = Path('/mnt/2021_NIA_data/jointattention')
 DATA_PATH    = ROOT_PATH.joinpath("raw_data_bgr").joinpath(task_name.lower())
 PROC_PATH    = ROOT_PATH.joinpath("PROC_DATA").joinpath(task_name.lower())
 if target_column == 'label' : 
-    PROJECT_PATH = Path(f'BINARY_FOLD_{data_ratio_name}_{target_column}').joinpath(task_name).joinpath(f'fold_{target_fold_num}')
+    PROJECT_PATH = Path(f'BINARY_FOLD_{target_column}').joinpath(task_name).joinpath(f'fold_{target_fold_num}')
     OUTPUT_SIZE = 2
     DROPOUT_RATIO = 0.5
 else :
-    PROJECT_PATH = Path(f'MULTI_FOLD_{data_ratio_name}_{target_column}').joinpath(task_name).joinpath(f'fold_{target_fold_num}')
+    PROJECT_PATH = Path(f'MULTI_FOLD_{target_column}').joinpath(task_name).joinpath(f'fold_{target_fold_num}')
     OUTPUT_SIZE = 3
     DROPOUT_RATIO = 0.1
 DF_PATH      = PROJECT_PATH.parent.joinpath("participant_information_df.csv")
